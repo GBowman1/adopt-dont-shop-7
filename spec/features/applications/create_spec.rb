c738131a61005ded53f7a84874d4558037cdff3d
Chee Lee <cheeleertr@gmail.com>
2024-05-17 16:03:40 -0500

    Story 3, add test for incomplete application form

--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -26,5 +26,17 @@ describe "Application Create" do
       expect(page).to have_current_path("/applications/show")
       expect(page).to have_content("Name: Chee")
     end
+
+    it "shows error when I fail to fill any form field" do
+      visit "/applications/new"
+
+      within "#applicant_form" do
+      fill_in "Name", with: "Chee"
+      end
+      click_button "Submit"
+
+      expect(page).to have_current_path("/applications/new")
+      expect(page).to have_content("You must fill in all fields")
+    end
   end
 end
