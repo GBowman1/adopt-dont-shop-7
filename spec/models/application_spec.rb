bee99d0bdaca7a02e70fc11f8e2e892ee07f5bad
Garrett Bowman <garrettbowman248@gmail.com>
2024-05-20 11:57:18 -0500

    Added model testing for methods

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Application, type: :model do
     describe "relationships" do
         it { should have_many :application_pets}
         it { should have_many(:pets).through(:application_pets) }
+        it { should validate_presence_of :name}
+        it { should validate_presence_of :street_address}
     end
 
     describe 'instance methods' do
@@ -27,6 +29,50 @@ RSpec.describe Application, type: :model do
             @app.add_pet(@hazel)
 
             expect(@app.pets).to eq([@hazel])
+        end
+        it 'can approve pet' do
+            @app = Application.create!(
+                name: "Chee Lee",
+                street_address: "123 street",
+                city: "New Orleans",
+                state: "Louisana",
+                zip_code: "12345",
+                description: "I will love that dog till I die",
+                status: "In Progress",
+            )
+            @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+            @hazel = @shelter_1.pets.create!(
+                name: "Hazel",
+                breed: "German Shepherd",
+                age: 5,
+                adoptable: true
+            )
+            @app.add_pet(@hazel)
+            @app.approve_pet(@hazel)
+
+            expect(@app.application_pets.first.approved).to eq(true)
+        end
+        it 'can reject pet' do
+            @app = Application.create!(
+                name: "Chee Lee",
+                street_address: "123 street",
+                city: "New Orleans",
+                state: "Louisana",
+                zip_code: "12345",
+                description: "I will love that dog till I die",
+                status: "In Progress",
+            )
+            @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+            @hazel = @shelter_1.pets.create!(
+                name: "Hazel",
+                breed: "German Shepherd",
+                age: 5,
+                adoptable: true
+            )
+            @app.add_pet(@hazel)
+            @app.reject_pet(@hazel)
+
+            expect(@app.application_pets.first.approved).to eq(false)
         end
     end
 end
